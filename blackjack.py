0a1d9715489919bfb48a309e090bb2b7754dccd2
g-lamonov <owsrey@gmail.com>
2018-08-16 03:32:57 +0700

    commit1

---
+++ blackjack.py
@@ -0,0 +1,13 @@
+
+class Card(object):
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+class Deck(object):
+    def __init__(self):
+
+
+
+class Player(object):
+    def __init__(self):
