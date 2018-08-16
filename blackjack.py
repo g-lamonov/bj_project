78e129beaf9b4ac62bda75c57f66457bdc3067df
g-lamonov <owsrey@gmail.com>
2018-08-16 07:16:33 +0700

    asd

--- blackjack.py
+++ blackjack.py
@@ -4,16 +4,21 @@ class Card(object):
         self.suit = suit
         self.value = value
 
+    def show(self):
+        print "{} of {}".format(self.value, self.suit)
 
 
 
 class Deck(object):
     def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for sui in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for valu in range(1, 14):
+                print "{} of {}".format(valu, sui)
 
 
 
-
-
-
-class Player(object):
-    def __init__(self):
+deck = Deck()
