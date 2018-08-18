18e1cd97c31f6a4096b46ddcbc0797692a870511
g-lamonov <owsrey@gmail.com>
2018-08-18 20:23:44 +0700

    change #5

--- blackjack.py
+++ blackjack.py
@@ -2,17 +2,40 @@
 class Card(object):
     def __init__(self, suit, value):
         self.suit = suit
-        self.value = value
+        self.rank = rank
 
     def show(self):
         print "{} of {}".format(self.value, self.suit)
 
+    def card_value(self):
+        if self.rank in "TJQK":
+            return 10
+        else:
+            return "A23456789".index(self.rank)
+
+class Hand(object):
+    def __init__(self, name):
+        self.name = name
+        self.cards = []                 # в руке ничего
+    def add_card(self, card):
+        self.cards.append(card)
+        result = 0
+        aces = 0
+        for card in self.cards:
+            result += card.card_value()
+            if card.get_rank() = "A":
+                aces += 1
+        if result + aces*10 <= 21:
+            result += aces*10
+        return result
+
+    def __str__(self):
 
 
 class Deck(object):
     def __init__(self):
         self.cards = []
-        
+
 
 
     def build(self):
