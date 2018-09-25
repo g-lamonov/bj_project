0db255007a50cd91a25e3593294548a2c388ecbb
g-lamonov <owsrey@gmail.com>
2018-09-25 18:27:10 +0700

    add class for work with JSON

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf-8
-
 import json
+from random import shuffle
+class Card(object):
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def card_value(self):
+        if self.rank in ('Валет','Королева','Король','Туз'):
+            return 10
+        else:
+            return ('', 'Туз','2','3','4','5','6','7','8','9','10').index(self.rank)
+    def get_rank(self):
+        return self.rank
+
+    def __str__(self):
+        return "%s%s"  % (self.rank, self. suit)
+
+class Deck(object):
+    def __init__(self):
+        ranks = ('2','3','4','5','6','7','8','9','10','Валет','Королева','Король','Туз')
+        suits = ('♦','♥','♣','♠')
+        self.cards = [Card(r,s) for r in ranks for s in suits]
+        shuffle(self.cards)
+
+    def deal_card(self):
+        return self.cards.pop()
+
+class WorkWithJSON:
+
+    def ChangeHandPlayer(self, data):
+        dataPlayer = self.OpenFile()
+
+        for key in dataPlayer["action2"]["playerHand"]:
+            if dataPlayer["action2"]["playerHand"][key] == 0:
+                dataPlayer["action2"]["playerHand"][key] = data
+                break
+
+    def OpenFile(self):
+        with open('gameHistory.json', 'r') as f:
+            data = json.loads(f.read())
+            return data
+
+    def WriteFile(self, data):
+        with open('gameHistory.json', 'w') as f:
+            json.dump(data, f)
+
+
+
+class Hand(object):
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+        WorkWithJSON().ChangeHandPlayer(card)
+
+
+    def get_value(self):
+        result = 0
+        aces = 0
+        for card in self.cards:
+            result += card.card_value()
+            if card.get_rank() == "Туз":
+                aces += 1
+        if result + aces * 10 <= 21:
+            result +=aces * 10
+        return result
+
+    def __str__(self):
+        text = "%sсодержит :\n" % self.name
+        for card in self.cards:
+            text += str(card) + " "
+        text += "\nЗначение на руке: " + str(self.get_value())
+
+        return text
+
+
+
 class GameHistory:
     def __init__(self):
         self.dct = {"game": {"state1": "0", "state2": "0", "state3": "0", "state4": "0"},
@@ -12,23 +90,23 @@ class GameHistory:
 
     def jsonName(self):
         return self.dct["action1"]["namePlayer"]
-    def jsonBalance(self):
-        return ("500")
 
     def new_game():
-        dct = {"game": {"state1": 0, "state2": 0, "state3": 0, "state4": 0,
-                  "state5": 0, "state6": 0, "state7": 0, "state8": 0},
-         "action1": {"id": None, "namePlayer": None, "balancePlayer": None},
-         "action2": {"playerHand": None},
-         "action3": {"dealerHand": None}}
+        dct = {"game" : {"state1" : 0, "state2" : 0 ,"state3" : 0, "state4" : 0,
+                "state5" : 0, "state6" : 0 ,"state7" : 0, "state8" : 0},
+                "playerData" : {"id": None,"namePlayer" : None, "balancePlayer" : None},
+                "action2" : {"playerHand": {"1": None , "2" : None, 3 : 0,
+                               "4" :0, "5" : 0, "6" : 0,
+                               "7" : 0, "8" : 0, "9" : 0}},
+                "action3" : {"dealerHand" :{"1": [None]}},
+                "statistics" : {"numberOfWins" : 15, "numberOfLosers": 10}
+               }
 
 class Game():
 
     def new_game(self):
-
-        with open('gameHistory.json', 'r') as f:
-            data = json.loads(f.read())
-
+        reading = WorkWithJSON()
+        data = reading.OpenFile()
         for key in data["game"]:
             if data["game"][key] == 1:
                 query = input("Желаете продолжить игру с прошлого момента? (y/n) \n")
@@ -40,9 +118,14 @@ class Game():
                     if data["game"]["state3"] == 1:
                         pass
 
-
                 if query == 'n':
-                    break
+                    self.Authorization()
+        self.Authorization()
+
+
+    def Authorization(self):
+        reading = WorkWithJSON()
+        data = reading.OpenFile()
 
         while True:
             answerToTheQuestion = int(input(
@@ -83,7 +166,6 @@ class Game():
                                           "Количество проигршей: %d\n")
                     print(templateForTheUser % userInformation)
                 continue
-
     def Bet(self):
         print("i'm here")
 
